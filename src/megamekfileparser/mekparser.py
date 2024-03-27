3663dfacf2070503d91e12eb9b1fe9a586466550
Chris Gerrish <c3gerris@gmail.com>
2024-03-26 20:46:50 -0400

    added "no file path" error. Also changed most class methods to private.

--- src/megamekfileparser/mekparser.py
+++ src/megamekfileparser/mekparser.py
@@ -10,14 +10,18 @@ from megamekfileparser.utils.base.weapon_locations import weapon_location_lookup
 class MekParser:
     def __init__(self, mtffilepath):
 
-        self.filepath: pathlib.Path = mtffilepath
+        if mtffilepath is None:
+            self.mtffilepath = None
+        else:
+            self.filepath: pathlib.Path = mtffilepath
+
         self.armor = {}
         self.weapons = {}
         self.equipment = {}
         self.fluff = {}
         self.fluff_keys = ['history', 'deployment', 'capabilities', 'overview', 'capabilities', 'manufacturer',
                            'systemmanufacturer', 'primaryfactory', 'systemmode']
-        self.config = self.get_config()
+        self.config = self.__get_config()
         self.systemmanufacturer = {}
 
         self.mech = OrderedDict()
@@ -26,7 +30,7 @@ class MekParser:
             self.mech_locs = [loc for _, loc in locs.items()]
 
     @staticmethod
-    def get_item(line: str, direction: Optional[str] = 'r') -> Optional[str]:
+    def __get_item(line: str, direction: Optional[str] = 'r') -> Optional[str]:
         match direction:
             case 'r':
                 out = line.split(":")[1].rstrip("\n")
@@ -38,7 +42,7 @@ class MekParser:
                 out = line.rstrip("\n")
                 return out
 
-    def parse_locations(self, equipment_location, file):
+    def __parse_locations(self, equipment_location, file):
         equipment = []
         line = file.readline()
         while line != "\n":
@@ -52,7 +56,7 @@ class MekParser:
             continue
         self.equipment.update({equipment_location.strip().lower(): equipment})
 
-    def get_config(self):
+    def __get_config(self):
 
         try:
             with open(file=self.filepath, encoding='utf8', errors='ignore', mode='r') as f:
@@ -65,14 +69,17 @@ class MekParser:
         return config
 
     def parse(self):
+        if self.filepath is None:
+            raise Exception('No filepath provided!')
+
         with open(file=self.filepath, encoding='utf8', errors='ignore', mode='r') as f:
             self.mech.update({'file': self.filepath.name})
             line = f.readline()
-            self.mech.update({'version': self.get_item(line, 'r')})
+            self.mech.update({'version': self.__get_item(line, 'r')})
             line = f.__next__()
-            self.mech.update({'chassis': self.get_item(line, 'n').strip().lower()})
+            self.mech.update({'chassis': self.__get_item(line, 'n').strip().lower()})
             line = f.__next__()
-            self.mech.update({'model': self.get_item(line, 'n').strip().lower()})
+            self.mech.update({'model': self.__get_item(line, 'n').strip().lower()})
 
             while line := f.readline():
 
@@ -94,16 +101,16 @@ class MekParser:
                         if "armor" in ln:
 
                             if ln.startswith('armor'):
-                                self.armor.update({"type": self.get_item(ln, 'r')})
+                                self.armor.update({"type": self.__get_item(ln, 'r')})
                             else:
-                                armor_location, armor_value = (self.get_item(ln, 'l'), self.get_item(ln, 'r'))
+                                armor_location, armor_value = (self.__get_item(ln, 'l'), self.__get_item(ln, 'r'))
                                 armor_location_check = armor_location.split(' ')
                                 armor_key = armor_config_lookup[self.config](self.config, armor_location_check[0])
                                 if armor_key:
                                     self.armor.update({armor_key: armor_value})
 
                         elif "weapons:" in ln:
-                            scans: int = int(self.get_item(ln, 'r'))
+                            scans: int = int(self.__get_item(ln, 'r'))
 
                             weapons_locations = copy.deepcopy(weapon_location_lookup[self.config])
 
@@ -133,7 +140,7 @@ class MekParser:
                                     self.weapons.update({weapon_location: weapon})
 
                         elif ln.replace(":", "") in self.mech_locs:
-                            self.parse_locations(ln, file=f)
+                            self.__parse_locations(ln, file=f)
 
                         else:
                             items = [i for i in line.split(":") if i != ""]
