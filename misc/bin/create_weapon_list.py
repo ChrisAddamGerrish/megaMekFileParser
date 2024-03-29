abf40684b5a1c59059e04f9a65df8efeff0ce817
Chris Gerrish <c3gerris@gmail.com>
2024-03-29 10:29:05 -0400

    Continuing to refine conversions, 30 more to go.

--- misc/bin/create_weapon_list.py
+++ misc/bin/create_weapon_list.py
@@ -12,19 +12,21 @@ for ids, mechData in data.items():
         if list:
             for i in list:
                 i = i.replace("(r)", "")
-                if equipment_alias.get(i):
+                if equipment_alias.get(i.replace(" ", "")):
                     continue
-
-                good, bad = mtf_weapon_alias(i)
-                if good:
-                    ok_weapon_list.add(good)
-                if bad:
-                    bad_weapon_list.add(bad)
+                else:
+                    good, bad = mtf_weapon_alias(i)
+                    if good:
+                        ok_weapon_list.add(good)
+                    if bad:
+                        bad_weapon_list.add(bad)
 
 print(len(ok_weapon_list))
 print(ok_weapon_list)
 print(len(bad_weapon_list))
 print(bad_weapon_list)
+
+
 
 with open('/Users/chris/Desktop/megamekparser/misc/static/badweapons.csv', 'w') as csvfile:
     for i in bad_weapon_list:
